bffcaadb2466c0c0ab937feb2859476c65352771
GUNGODD <chauhanadityac01@gmail.com>
2024-02-20 00:15:35 +0530

    finally

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,20 +1,29 @@
 // Board.jsx
-
+"use client";
 import React from 'react';
-import Column from "./Column";
+import Column from './Columns';
 import NewColumnForm from "./forms/NewColumnForm";
 
 const columns = [
     { id: 'Aditya', name: 'todo', index: 0 },
     { id: 'Anurag', name: 'in progress', index: 1 },
     { id: 'Chota bheem', name: 'done', index: 2 },
+    
 ];
 
-const cards = [
-    { id: "Aditya", name: "Task 1", order: 0 },
-    { id: "Anjali", name: "Task 2", order: 1 },
-    { id: "Anurag", name: "Task 3", order: 2 },
-    { id: "Rashi", name: "Task 4", order: 3 },
+
+export type CardType = {
+   
+        name: String;
+        id:String;
+        order:Number;
+  
+}
+export const cards = [
+    { id: "Aditya", name: "Task 1", order: 0, columnId: 'Aditya' },
+    { id: "Anjali", name: "Task 2", order: 1, columnId: 'Anurag' },
+    { id: "Anurag", name: "Task 3", order: 2, columnId: 'Chota bheem' },
+
 ];
 
 export default function Board() {
@@ -22,7 +31,9 @@ export default function Board() {
         <>
             <div className="flex gap-4">
                 {columns.map(column => (
-                    <Column key={column.index} name={column.name} cards={cards} />
+        <div className="w-48 bg-white shadow-sm rounded-md p-2">
+                      <Column  name={...Column} cards={cards.filter(c =>c.columnId === column.id)} />
+        </div>
                 ))}
                 <NewColumnForm />
             </div>
