3bb50fc8dc211a674aa8a17eb0efc8defe87840e
GUNGODD <chauhanadityac01@gmail.com>
2024-02-19 19:25:37 +0530

    cards fixed

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,14 +2,24 @@ type ColumnProps = {
     key: number,
     name: String,
 
-}
+}; // do not make any typos 
 
-export default  function  Column ({key ,name}:ColumnProps){
+const cards = [
+    { id: "Aditya", name: "Task 1", order: 0 },
+    { id: "Anjali", name: "Task 2", order: 1 },
+    { id: "Anurag ", name: "Task 3", order: 2 },
+    { id: "Rashi", name: "Task 4", order: 3 },
+]
+
+
+
+
+export default function Column({ key, name }: ColumnProps) {
     return (
         <>
-     <div key={key} className="w-36 bg-white shadow-sm rounded-md p-2">
-                    {name}
-                </div>
+            <div key={key} className="w-36 bg-white shadow-sm rounded-md p-2">
+                {name}
+            </div>
 
         </>
     );
