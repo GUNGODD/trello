5a63773992d33b7a8a83a3e80b7fdaee1b80a1a7
GUNGODD <chauhanadityac01@gmail.com>
2024-02-20 22:45:10 +0530

    React Sort added to the projec t

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,5 @@
-
+"use client"
+import { useState } from "react";
 import Column from "./Column";
 import NewColumnForm from "./forms/NewColumnForm";
 
@@ -10,7 +11,7 @@ export type  CardType = {
     name: string;
     index: number;
 }
-const columns  = [
+const defaultColumns  = [
 
     {id: 'sxsd', name: 'todo', index:0},
     {id: 'ppdf', name: 'in progress', index:1},
@@ -24,24 +25,27 @@ name:String,
 order:Number,
 };
 
- const cards = [ 
+ const defaultCards = [ 
 
-    { id: "1", name: "Task 1", order: 1 },
-    { id: "2", name: "Task 2", order: 2 },
-    { id: "3", name: "Task 3", order: 3 },
-    { id: "4", name: "Task 4", order: 4 },
-    { id: "5", name: "Task 5", order: 5 },
+    { id: "1", name: "Task 1", order: 1, columnId:"sxsd" },
+    { id: "2", name: "Task 2", order: 2, columnId:"ppdf" },
+    { id: "3", name: "Task 3", order: 3, columnId:"sxsd" },
+    { id: "4", name: "Task 4", order: 4, columnId:"kdxd" },
+    { id: "5", name: "Task 5", order: 5, columnId: "kdxk"},
     
 ];
 
 export default  function Board(){
+    const [cards, setCards] = useState(defaultCards);
+    const [columns, setColumns] = useState(defaultColumns);
     return (
         <>
 
         <div className="flex  gap-4">
             {columns.map(column =>(
                 // eslint-disable-next-line react/jsx-key
-           <Column {...column} cards={cards} />
+           <Column {...column} 
+           cards ={cards.filter(c => c.columnId === column.id)} />
              
             ))}
         </div>
