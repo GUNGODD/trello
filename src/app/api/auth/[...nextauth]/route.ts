bdc7ebb9d74c18f4b1b5849ad0395a8518d722f3
GUNGODD <chauhanadityac01@gmail.com>
2024-02-21 01:30:17 +0530

    adapter setup completed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import {MongoDBAdapter} from '@next-auth/mongodb-adapter'
 const handler = NextAuth({
  providers : [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      })
+      }),
 
- ]
-})
+ ],
+ // adapter 
+  adapter: MongoDBAdapter(clientPromise),
+});
 
 export { handler as GET, handler as POST }
