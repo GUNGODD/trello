9a468d330b24c9c0e397c9e3adcb830767bfa5fe
GUNGODD <chauhanadityac01@gmail.com>
2024-02-22 21:17:46 +0530

    fixed pacakge.json files

--- src/app/new-board/page.tsx
+++ src/app/new-board/page.tsx
@@ -1,28 +1,22 @@
-"use client";
+'use client';
+import {createBoard} from "@/app/actions/boardActions";
+import {redirect} from "next/navigation";
 
-import { redirect } from "next/dist/server/api-utils";
-import createBoard from "../actions/boardActions";
-
-export default function NewBoardPage(){
-   async  function handleNewBoardSubmit(formData: FormData) {
-const boardName = formData.get('name')?.toString() || '';
-
-// or 
-// await createBoard(boardName as string); 
-     const {id} =   await createBoard(boardName);
-     redirect(`/boards/${id}`);
+export default function NewBoardPage() {
+  async function handleNewBoardSubmit(formData: FormData) {
+    const boardName = formData.get('name')?.toString() || '';
+    const roomInfo = await createBoard(boardName);
+    if (roomInfo) {
+      redirect(`/boards/${roomInfo.id}`);
     }
-    
-    return (
-       <div>
-        <form action={handleNewBoardSubmit} className="mx-w-xs block">
-            <h1 className="text-2xl mb-4">Create new boards</h1>
-            <input type="text" placeholder="board name  " />
-            <button className="mt-2 w-full" type="submit">Create board</button>
-        </form>
-
-
-       </div>
-    );
+  }
+  return (
+    <div>
+      <form action={handleNewBoardSubmit} className="max-w-xs block">
+        <h1 className="text-2xl mb-4">Create new board</h1>
+        <input type="text" name="name" placeholder="board name"/>
+        <button type="submit" className="mt-2 w-full">Create board</button>
+      </form>
+    </div>
+  );
 }
-
