fdaef68dbbc178acfbde2677b030dc644573a6a9
GUNGODD <chauhanadityac01@gmail.com>
2024-02-22 18:42:30 +0530

    Merge branch 'Saya'

---
+++ src/app/actions/boardActions.tsx
@@ -0,0 +1,4 @@
+export default function createBoard(name:string ){
+
+    return {};
+}
--- src/app/new-board/page.tsx
+++ src/app/new-board/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import createBoard from "../actions/boardActions";
+
 export default function NewBoardPage(){
+   async  function handleNewBoardSubmit(formData: FormData) {
+const boardName = formData.get('name')?.toString() || '';
+
+// or 
+// await createBoard(boardName as string); 
+       
+    }
+    
     return (
-        <>
-        <div>new board page</div>
-      
-        
-        </>
+       <div>
+        <form action={handleNewBoardSubmit} className="mx-w-xs block">
+            <h1 className="text-2xl mb-4">Create new boards</h1>
+            <input type="text" placeholder="board name  " />
+            <button className="mt-2 w-full" type="submit">Create board</button>
+        </form>
+
+
+       </div>
     );
 }
+
