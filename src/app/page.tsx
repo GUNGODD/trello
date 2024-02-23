6890b6184e8a68286f87853286bf565d8c566b90
GUNGODD <chauhanadityac01@gmail.com>
2024-02-23 06:59:58 +0530

    liveblocks client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,29 @@
-
-import Board from "@/components/Board";
+import Boards from "@/components/Boards";
 import LoginView from "@/components/views/LoginView";
-import { authOptions } from "@/lib/authOptions";
-import { faArrowRightArrowLeft, faRightLong } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { getServerSession } from "next-auth";
-import { getSession } from "next-auth/react";
+import {authOptions} from "@/lib/authOptions";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {getServerSession} from "next-auth";
 import Link from "next/link";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-    if (!session) {
-      return (
-          <LoginView />
-      );
-    }
+  if (!session) {
+    return (
+      <LoginView />
+    );
+  }
   return (
-    <>
     <div>
-    <h1 className="text-4xl mb-4">Your Borads</h1>
-  
-
-
-{/* <Board /> */}
-
-<div>
-<Link  className="btn primary inline-flex gap-1 "
-href={'/new-board'}>
-  Create new Board  <FontAwesomeIcon className="h-6" icon={faRightLong} />
-  </Link>
-
-</div>
-
-
+      <h1 className="text-4xl mb-4">Your boards</h1>
+      <Boards/>
+      <div className="mt-4">
+        <Link
+          className="btn primary inline-flex gap-2"
+          href={'/new-board'}>
+          Create new board <FontAwesomeIcon className="h-6" icon={faArrowRight}/>
+        </Link>
+      </div>
     </div>
-    </>
-  );
+  )
 }
