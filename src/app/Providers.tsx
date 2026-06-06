c19ba01408ced7b14cec755207405b3d2ac09e57
chromastone <chauhanadityac01@gmail.com>
2026-06-07 00:41:47 +0530

    Wrap Providers with SessionProvider from next-auth/react to fix infinite loading session screen

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,13 +3,16 @@
 import { ReactNode } from "react";
 import { LiveblocksProvider } from "@liveblocks/react/suspense";
 import { ThemeProvider } from "@/components/ThemeContext";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
-    </LiveblocksProvider>
+    <SessionProvider>
+      <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </LiveblocksProvider>
+    </SessionProvider>
   );
 }
